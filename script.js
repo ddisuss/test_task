af127a7261dd6883ea953a1cb5cd8fbed7b72a9a
ddisuss <131768761+ddisuss@users.noreply.github.com>
2023-12-15 17:08:42 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,51 @@
+const gallary = new Swiper('.gallary', {
+
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.btn-next',
+      prevEl: '.btn-prev',
+    },
+    // If we need pagination
+    pagination: {
+        el: '.pagination',
+        clickable:true,
+      },
+      slidesPerView: 3,
+      spaceBetween: 0,
+  });
+const gallary1 = new Swiper('.gallary1', {
+
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.btn-next1',
+      prevEl: '.btn-prev1',
+    },
+    // If we need pagination
+    pagination: {
+        el: '.pagination1',
+        clickable:true,
+      },
+      slidesPerView: 4,
+      spaceBetween: 0,
+  });
+const gallary2 = new Swiper('.gallary2', {
+
+
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.btn-next2',
+      prevEl: '.btn-prev2',
+    },
+    // If we need pagination
+    pagination: {
+        el: '.pagination2',
+        clickable:true,
+      },
+      slidesPerView: 1,
+      spaceBetween: 0,
+  });
